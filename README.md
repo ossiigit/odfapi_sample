c6ae50fb0e85d23d744d5b3502abe1a51f8a162d
nienzu <nientorec@gmail.com>
2019-07-12 18:34:15 +0800

    國發會 API 範例網:仲介履歷 & 報價單
    出版網站，實作基本應用還有一個基本功能兩個進階應用待實作

---
+++ README.md
@@ -0,0 +1,12 @@
+# 國發會 ODF API 環境建置
+
+### How to setup?
+```
+pip install -r requirements.txt
+```
+
+### How to run?
+
+```
+python server.py
+```
--- server.py
+++ server.py
@@ -1,8 +1,43 @@
-from flask import Flask, escape, request
+# -*- coding: UTF-8 -*-
 
+from flask import Flask, escape, request, render_template, send_from_directory
+import requests, json
+from datetime import datetime
 app = Flask(__name__)
-
+apilist={
+    '履歷表生成範例':'http://192.168.3.186:9980/lool/merge-to/10db8056-a485-11e9-8e17-080027f6714d',
+    '報價單生成範例':'http://192.168.3.186:9980/lool/merge-to/1c934f06-a48c-11e9-8e17-080027f6714d',
+    'salary':'',
+}
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+def landing():
+    return render_template('index.html')
+    
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/price')
+def price():
+    return render_template('price.html')
+
+@app.route('/salary')
+def salary():
+    return render_template('salary.html')
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    api = request.form['apiname']
+    jsonData = request.form['userinput好']
+    res = requests.post(apilist[api],json=json.loads(jsonData))
+    filename = api + str(datetime.now()).replace(' ','').replace(':','')
+    with open(f'file/{filename}.odt', 'wb') as f :
+        f.write(res.content)
+    return render_template('result.html', link=f'{filename}.odt')
+
+@app.route('/download/<path:filename>', methods=['POST', 'GET'])
+def download(filename):
+    return send_from_directory(directory='file', filename=filename)
+
+if __name__ == "__main__":
+    app.run(port="80", debug=True)
