8a12e18289772ce49a9476a8714f64c29818b06e
nienzu <nientorec@gmail.com>
2019-07-18 18:34:03 +0800

    ODF API 範例網站初版
    目前共製作兩種範例 json/表單, 分別是履歷, 報價單
    
    conf : webapi 設定檔
    範例報表 : 已製作好的檔案
    file : response 回來的檔案

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-# 國發會 ODF API 環境建置
+# ODF API Server 安裝
 
-### How to setup?
+要跑範例必須要先完成 ODF API Server 的建置詳情請參考[ODF文件API工具](https://www.ndc.gov.tw/cp.aspx?n=260AB6C70F5AF292&s=04C4AB150E9149D6)
+
+
+# ODF API 範例網頁環境
+
+### 如何部屬?
 ```
 pip install -r requirements.txt
 ```
 
-### How to run?
+### 如何執行?
+```
+python server.py --host=0.0.0.0 --port=8080
+```
+
+### 如何修改 ODF API 網址?
+
+ODF API 的網址修改只需要到 `conf/odfapi.py`, 修改後面的網址(這邊採用的網址都是 JSON API)
 
 ```
-python server.py
+{
+    '履歷表生成範例':'http://192.168.3.186:9980/lool/merge-to/10db8056-a485-11e9-8e17-080027f6714d',
+    '報價單生成範例':'http://192.168.3.186:9980/lool/merge-to/1c934f06-a48c-11e9-8e17-080027f6714d',
+}
 ```
--- server.py
+++ server.py
@@ -3,12 +3,9 @@
 from flask import Flask, escape, request, render_template, send_from_directory
 import requests, json
 from datetime import datetime
+from conf.odfapi import apilist
 app = Flask(__name__)
-apilist={
-    '履歷表生成範例':'http://192.168.3.186:9980/lool/merge-to/10db8056-a485-11e9-8e17-080027f6714d',
-    '報價單生成範例':'http://192.168.3.186:9980/lool/merge-to/1c934f06-a48c-11e9-8e17-080027f6714d',
-    'salary':'',
-}
+
 @app.route('/')
 def landing():
     return render_template('index.html')
@@ -21,9 +18,9 @@ def resume():
 def price():
     return render_template('price.html')
 
-@app.route('/salary')
-def salary():
-    return render_template('salary.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/result', methods=['POST', 'GET'])
 def result():
@@ -35,9 +32,40 @@ def result():
         f.write(res.content)
     return render_template('result.html', link=f'{filename}.odt')
 
+@app.route('/result2', methods=['POST', 'GET'])
+def result2():
+    api = request.form['apiname']
+    formData = dict(request.form)
+    # 手動拆解群組資料指定給 JSON 報價單 API 的 項目變數 
+    itemDict = {
+        
+    }
+    jsonData={}
+    for key in formData:
+        if 'item' in key:
+            keyList = key.split('-')
+            if keyList[1] not in itemDict:
+                itemDict[keyList[1]] = {}
+            itemDict[keyList[1]][keyList[2]] = formData[key]
+        else:
+            jsonData[key] = formData[key]
+
+    jsonData['項目']=[]
+    totalPrice = 0
+    for item in itemDict:
+        jsonData['項目'].append(itemDict[item])
+        totalPrice += int(itemDict[item]['小計'])
+    jsonData['總計'] = totalPrice
+
+    res = requests.post(apilist[api], json=jsonData)
+    filename = api + str(datetime.now()).replace(' ','').replace(':','')
+    with open(f'file/{filename}.odt', 'wb') as f :
+        f.write(res.content)
+    return render_template('result.html', link=f'{filename}.odt')
+
 @app.route('/download/<path:filename>', methods=['POST', 'GET'])
 def download(filename):
     return send_from_directory(directory='file', filename=filename)
 
 if __name__ == "__main__":
-    app.run(port="80", debug=True)
+    app.run(host='0.0.0.0',port="80", debug=True)
